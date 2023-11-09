7c2d078b1ab1fc850f457a9e42d23481cd30688c
Anh Hach <hachanhvu2205@gmail.com>
2023-11-09 18:13:40 +0000

    ok

--- steering.py
+++ steering.py
@@ -1,56 +1,79 @@
-import cv2
+from dronekit import connect, VehicleMode, mavutil
 import pyrealsense2 as rs
 import time
 import numpy as np
+import cv2
+
+# Connect to the vehicle
+vehicle = connect('/dev/ttyAMA0', wait_ready=True, baud=57600)
+
+# Function to override RC channels
+def override_rc(channels):
+    
+    channel_values = [0]*8  # there are eight RC channels on most systems
+    for channel, value in channels.items():
+        channel_values[channel-1] = value  # channels are 1-indexed in MAVLink
+    vehicle.channels.overrides = channel_values
+
+# Function to clear RC overrides
+def clear_rc_overrides():
+    
+    vehicle.channels.overrides = {}
 
 # Initialize the RealSense pipeline
 pipeline = rs.pipeline()
 config = rs.config()
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)     # Depth stream
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)  # Depth stream
 pipeline.start(config)
-# Get the depth stream profile to access stream properties
-depth_stream = pipeline.get_active_profile().get_stream(rs.stream.depth)
 
-# Get the width of the depth stream
-depth_width = depth_stream.as_video_stream_profile().get_intrinsics().width
-print(depth_width)
+# Function to check for obstacles and avoid them
+def check_and_avoid_obstacles():
+    # Wait for the next set of frames from the camera
+    frames = pipeline.wait_for_frames()
+    depth_frame = frames.get_depth_frame()
+    if not depth_frame:
+        return
+
+    # Convert the depth frame to a numpy array
+    depth_image = np.asanyarray(depth_frame.get_data())
+    # Assuming the obstacle is in front of the camera, get the depth value in the center of the image
+    obstacle_distance = depth_image[depth_image.shape[0] // 2, depth_image.shape[1] // 2]
+
+    # Define a threshold distance in meters (1 meter in this case)
+    threshold_distance_m = 1.0
+
+    # If there's an obstacle within the threshold distance, avoid it
+    if obstacle_distance < threshold_distance_m * 1000:  # Convert meters to the scale of depth values
+        if vehicle.mode.name != "GUIDED":
+            print("Obstacle detected! Switching to GUIDED mode and overriding RC.")
+            vehicle.mode = VehicleMode("GUIDED")
+            time.sleep(1)  # Give some time to switch to GUIDED mode
+            override_rc({5: 1680})  
+        else:
+            print("Already in GUIDED mode, overriding RC.")
+            override_rc({5: 1680})  # Continue overriding RC commands
+    else:
+        if vehicle.mode.name == "GUIDED":
+            print("No obstacles detected. Returning to AUTO mode and clearing RC overrides.")
+            vehicle.mode = VehicleMode("AUTO")
+            clear_rc_overrides()
+        elif vehicle.mode.name == "AUTO":
+            # If in AUTO mode and no overrides are active, keep checking for obstacles
+            if vehicle.channels.overrides:
+                clear_rc_overrides()
+
 try:
     while True:
-        # Wait for the next frame
-        frames = pipeline.wait_for_frames()
-
-        # Get the depth frame
-        depth_frame = frames.get_depth_frame()
-
-        if not depth_frame:
-            continue
-
-        # Convert depth frame to a numpy array
-        depth_image = np.asanyarray(depth_frame.get_data())
-        depth_image = cv2.convertScaleAbs(depth_image, alpha=0.03)  # Adjust depth visualization
-
-        column_means = np.mean(depth_image, axis=0)
-        print(len(column_means))
-        # Find the index of the column with the highest mean
-        index_of_highest_mean = np.argmax(column_means)
-        print("Index of column with highest mean:", index_of_highest_mean)
-        print("angle:",index_of_highest_mean/len(column_means)*87-(87/2))
-        # Display the depth image
-        # Apply a colormap to the depth image
-        color_depth_image = cv2.applyColorMap(depth_image, cv2.COLORMAP_JET)
-        cv2.imshow("Depth Image", color_depth_image)
-        cv2.waitKey(1)
-
-        # # Capture the depth image to a file (you can choose your own filename and format)
-        # timestamp = time.time()
-        # image_filename = f"depth_{int(timestamp)}.png"
-        # cv2.imwrite(image_filename, depth_image)
-
-        time.sleep(0.5)  # Capture every 1 second
+        check_and_avoid_obstacles()
+        time.sleep(0.1)
 
 except KeyboardInterrupt:
-    pass
+    print("Script terminated by user")
 
-# Release resources
-pipeline.stop()
-cv2.destroyAllWindows()
+finally:
+    # Clean up
+    clear_rc_overrides()
+    pipeline.stop()
+    cv2.destroyAllWindows()
+    vehicle.close()
+    print("Connection closed.")
