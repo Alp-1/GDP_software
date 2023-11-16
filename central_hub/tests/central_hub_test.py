c7d06d8b1f683944f6fa23e4267c66a92a977a9d
Shen Yik Chua <syc2e18@soton.ac.uk>
2023-11-16 22:55:24 +0000

    Update tests to use the latest protocols

--- central_hub/tests/central_hub_test.py
+++ central_hub/tests/central_hub_test.py
@@ -5,8 +5,15 @@ import struct
 import pytest
 
 
-with patch.dict("sys.modules", machine=Mock(), ustruct=struct, time=Mock()):
+with patch.dict(
+    "sys.modules",
+    machine=Mock(),
+    ustruct=struct,
+    time=Mock(),
+    rp2=Mock(),
+):
     import time
+    from protocol import Commands
     from central_hub.central_hub import CentralHub
 
 
@@ -24,14 +31,29 @@ def central_hub():
 
 
 @pytest.mark.parametrize(
-    "mode, commands, expected",
+    "mode, commands, expected_front, expected_rear",
     [
-        (CentralHub.DIRECT_RC, [80, 50], [-80, -80, 50]),
-        (CentralHub.FLIGHT_CONTROLLER, [70, 50, 80, 50], ([70, 50], [80, 50])),
-        (CentralHub.FULLY_AUTONOMOUS, [-50, -60, -30, -40], ([-50, -60], [-30, -40])),
+        (
+            CentralHub.DIRECT_RC,
+            [80, 50],
+            Commands.generate_command((Commands.SET_SPEED_MIXED, (-80, 50))),
+            Commands.generate_command((Commands.SET_SPEED_MIXED, (-80, 50))),
+        ),
+        (
+            CentralHub.FLIGHT_CONTROLLER,
+            [70, 50, 80, 50],
+            Commands.generate_command((Commands.SET_SPEED_LEFT_RIGHT, (70, 50))),
+            Commands.generate_command((Commands.SET_SPEED_LEFT_RIGHT, (80, 50))),
+        ),
+        (
+            CentralHub.FULLY_AUTONOMOUS,
+            [-50, -60, -30, -40],
+            Commands.generate_command((Commands.SET_SPEED_LEFT_RIGHT, (-50, -60))),
+            Commands.generate_command((Commands.SET_SPEED_LEFT_RIGHT, (-30, -40))),
+        ),
     ],
 )
-def test_update_command(central_hub, mode, commands, expected):
+def test_update_command(central_hub, mode, commands, expected_front, expected_rear):
     """Test if the update_command method writes the correct command depends on the mode"""
 
     central_hub.update_mode = Mock()
@@ -48,22 +70,14 @@ def test_update_command(central_hub, mode, commands, expected):
 
     central_hub.update_command()
 
-    if mode == CentralHub.DIRECT_RC:
+    if mode in [
+        CentralHub.DIRECT_RC,
+        CentralHub.FLIGHT_CONTROLLER,
+        CentralHub.FULLY_AUTONOMOUS,
+    ]:
         # In DIRECT_RC mode, the command should be the same as the RC receiver
-        central_hub.front_wheel_controller.write.assert_called_with(
-            struct.pack("fff", *expected)
-        )
-        central_hub.rear_wheel_controller.write.assert_called_with(
-            struct.pack("fff", *expected)
-        )
-    elif mode == CentralHub.FLIGHT_CONTROLLER or mode == CentralHub.FULLY_AUTONOMOUS:
-        # In FLIGHT_CONTROLLER mode, the command should be the same as the flight controller
-        central_hub.front_wheel_controller.write.assert_called_with(
-            struct.pack("ff", *expected[0])
-        )
-        central_hub.rear_wheel_controller.write.assert_called_with(
-            struct.pack("ff", *expected[1])
-        )
+        central_hub.front_wheel_controller.write.assert_called_with(expected_front)
+        central_hub.rear_wheel_controller.write.assert_called_with(expected_rear)
     else:
         raise Exception("Unexpected mode for this test")
 
@@ -76,8 +90,8 @@ def test_handshake_wait(central_hub):
     time.ticks_diff.return_value = 0
 
     # The handshake_command is offset by 1
-    handshake_command = bytes([1, 2, 3, 4, 5])
-    mock_read_data = bytes([0, *handshake_command]) * 10
+    handshake_command = Commands.ACK_COMMAND
+    mock_read_data = bytes([0, *Commands.ACK_COMMAND]) * 10
     iter_count = 0
 
     def mock_read(num_bytes=0):
@@ -91,14 +105,14 @@ def test_handshake_wait(central_hub):
 
     # ignore the front wheel controller
     central_hub.front_wheel_controller.any.return_value = True
-    central_hub.front_wheel_controller.read.side_effect = [handshake_command]*10
+    central_hub.front_wheel_controller.read.side_effect = [handshake_command] * 10
 
     # mock the rear wheel controller
     central_hub.rear_wheel_controller.any.return_value = True
     central_hub.rear_wheel_controller.read.side_effect = mock_read
 
     try:
-        central_hub.handshake_wait(handshake_command)
+        central_hub.handshake_wait()
     except Exception as e:
         pytest.fail("handshake_wait raised an exception: {}".format(e))
 
@@ -157,15 +171,13 @@ def test_fault_clear(central_hub, state_switch, mocker):
 def test_state_machine_transition(central_hub, initial_state, switch_sequence, mocker):
     """Test the state machine transitions"""
 
-    central_hub.enable_controllers = Mock()
-    central_hub.disable_controllers = Mock()
+    central_hub.configure_controllers = Mock()
     central_hub.fault_check = Mock()
     central_hub.fault_check.return_value = False
 
     def reset_mocks():
         """Reset the mocks"""
-        central_hub.enable_controllers.reset_mock()
-        central_hub.disable_controllers.reset_mock()
+        central_hub.configure_controllers.reset_mock()
 
     mocker.patch(
         __name__ + ".CentralHub.state_selector",
@@ -181,36 +193,28 @@ def test_state_machine_transition(central_hub, initial_state, switch_sequence, m
         central_hub.update_state()
         match (previous_state, previous_switch_state, current_switch_state):
             case (CentralHub.RUNNING, CentralHub.PAUSED, CentralHub.PAUSED):
-                central_hub.enable_controllers.assert_not_called()
-                central_hub.disable_controllers.assert_called_once()
+                central_hub.configure_controllers.assert_called_once_with(enable=False)
                 assert central_hub.state == CentralHub.PAUSED
             case (CentralHub.RUNNING, CentralHub.PAUSED, CentralHub.RUNNING):
-                central_hub.enable_controllers.assert_called_once()
-                central_hub.disable_controllers.assert_not_called()
+                central_hub.configure_controllers.assert_called_once_with(enable=True)
                 assert central_hub.state == CentralHub.RUNNING
             case (CentralHub.RUNNING, CentralHub.RUNNING, CentralHub.PAUSED):
-                central_hub.enable_controllers.assert_not_called()
-                central_hub.disable_controllers.assert_called_once()
+                central_hub.configure_controllers.assert_called_once_with(enable=False)
                 assert central_hub.state == CentralHub.PAUSED
             case (CentralHub.RUNNING, CentralHub.RUNNING, CentralHub.RUNNING):
-                central_hub.enable_controllers.assert_not_called()
-                central_hub.disable_controllers.assert_not_called()
+                central_hub.configure_controllers.assert_not_called()
                 assert central_hub.state == CentralHub.RUNNING
             case (CentralHub.PAUSED, CentralHub.PAUSED, CentralHub.PAUSED):
-                central_hub.enable_controllers.assert_not_called()
-                central_hub.disable_controllers.assert_not_called()
+                central_hub.configure_controllers.assert_not_called()
                 assert central_hub.state == CentralHub.PAUSED
             case (CentralHub.PAUSED, CentralHub.PAUSED, CentralHub.RUNNING):
-                central_hub.enable_controllers.assert_called_once()
-                central_hub.disable_controllers.assert_not_called()
+                central_hub.configure_controllers.assert_called_once_with(enable=True)
                 assert central_hub.state == CentralHub.RUNNING
             case (CentralHub.PAUSED, CentralHub.RUNNING, CentralHub.PAUSED):
-                central_hub.enable_controllers.assert_not_called()
-                central_hub.disable_controllers.assert_called_once()
+                central_hub.configure_controllers.assert_called_once_with(enable=False)
                 assert central_hub.state == CentralHub.PAUSED
             case (CentralHub.PAUSED, CentralHub.RUNNING, CentralHub.RUNNING):
-                central_hub.enable_controllers.assert_called_once()
-                central_hub.disable_controllers.assert_not_called()
+                central_hub.configure_controllers.assert_called_once_with(enable=True)
                 assert central_hub.state == CentralHub.RUNNING
             case _:
                 raise Exception("Unexpected state for this test")
