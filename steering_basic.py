68d37a7f7e5c66010aba42ee12411c6d1b170501
Dávid Máté <dm1e19@soton.ac.uk>
2023-11-26 21:57:20 +0000

    override rc channels

--- steering_basic.py
+++ steering_basic.py
@@ -17,6 +17,20 @@ vehicle = connect('/dev/serial0', wait_ready=True, baud=57600)
 # Global variable for the RealSense profile
 profile = None
 
+
+# Function to override RC channels
+def override_rc(channels):
+    channel_values = [0] * 8  # there are eight RC channels on most systems
+    for channel, value in channels.items():
+        channel_values[channel - 1] = value  # channels are 1-indexed in MAVLink
+    vehicle.channels.overrides = channel_values
+
+
+# Function to clear RC overrides
+def clear_rc_overrides():
+    vehicle.channels.overrides = {}
+
+
 # Initialize RealSense pipeline and profile
 def initialize_realsense():
     global profile
@@ -88,7 +102,7 @@ def navigate_avoiding_obstacles(depth_scale):
             print("Clear path found. Setting heading and moving forward.")
             vehicle.mode = VehicleMode("GUIDED")
             print(vehicle.mode.name)
-
+            override_rc({5: 1680})
             # Set the heading of the rover
             # set_position_target_local_ned(
             #     x=0, y=0, z=0,
@@ -158,6 +172,7 @@ except KeyboardInterrupt:
     print("Script terminated by user")
 
 finally:
+    clear_rc_overrides()
     pipeline.stop()
     cv2.destroyAllWindows()
     vehicle.close()
