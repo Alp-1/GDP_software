38584a57a428c7c055b06b6790ac3b18d5d4c273
David <dm1e19@dm1e19.soton.ac.uk>
2023-12-08 22:32:21 +0000

    pointcloud creation

--- geometric_map.py
+++ geometric_map.py
@@ -40,14 +40,32 @@ def fit_plane_to_point_cloud(point_cloud):
 
 
 def main():
-    o3d.t.io.RealSenseSensor.list_devices()
-    start_time = time.time()
-    x = np.random.random((200000, 3))
-    y = o3d.utility.Vector3dVector(x)
-    depth_frame = capture_depth_image()
-    depth_image = np.asanyarray(depth_frame.get_data())
-    pcd = o3d.geometry.PointCloud.create_from_depth_image(depth_image,o3d.camera.PinholeCameraIntrinsic)
-    print(pcd)
+    global profile
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_stream(rs.stream.color, 848, 480, rs.format.bgr8, 60)  # RGB stream
+    config.enable_stream(rs.stream.depth, 848, 480, rs.format.z16, 60)  # Depth stream
+    config.enable_stream(rs.stream.accel, rs.format.motion_xyz32f, 200)  # Accelerometer data
+    config.enable_stream(rs.stream.gyro, rs.format.motion_xyz32f, 400)  # Gyroscope data
+    profile = pipeline.start(config)
+    
+    depth_sensor = profile.get_device().first_depth_sensor()
+    depth_scale = depth_sensor.get_depth_scale()
+    while True:
+        frames = pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+
+        
+        start_time = time.time()
+        x = np.random.random((200000, 3))
+        y = o3d.utility.Vector3dVector(x)
+        depth_image = np.asanyarray(depth_frame.get_data())
+        pcd = o3d.geometry.PointCloud.create_from_depth_image(
+            o3d.geometry.Image(depth_image),o3d.camera.PinholeCameraIntrinsic(o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault
+            ),
+            depth_scale = depth_scale)
+        print(pcd)
+        time.sleep(1)
 
     print("--- %s seconds ---" % (time.time() - start_time))
 if __name__ == "__main__":
