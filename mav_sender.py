3421543f77ccb9e2595d5c73856161127146b936
Shen Yik Chua <syc2e18@soton.ac.uk>
2023-12-19 14:31:36 +0000

    Add functions to switch between different gps types

--- mav_sender.py
+++ mav_sender.py
@@ -1,9 +1,13 @@
 import time
 
-# 50.937472 -1.39575
+# 50.937472 -1.39575 is near B16
 FAKE_LONGITUDE = -1.39575
 FAKE_LATITUDE = 50.937472
 FAKE_ALTITUDE = 0.0
+
+# GPS_TYPE Enum
+GPS_TYPE_NMEA = 5
+GPS_TYPE_MAVLINK = 14
 
 def get_gps_time(tnow):
     '''return gps_week and gps_week_ms for current unix time in seconds'''
@@ -18,6 +22,19 @@ def get_gps_time(tnow):
     week_ms = (epoch_seconds % SEC_PER_WEEK) * 1000 + ((t_ms//200) * 200)
     return week, week_ms
 
+def switch_to_fake_gps(mavlink_connection):
+    """Change GPS_TYPE to MAVLink GPS. This method will reboot the autopilot so
+     that the change takes effect.
+    """
+    mavlink_connection.param_set_send("GPS_TYPE", GPS_TYPE_MAVLINK)
+    mavlink_connection.reboot_autopilot()
+
+def switch_to_real_gps(mavlink_connection):
+    """Change GPS_TYPE to NMEA GPS. This method will reboot the autopilot so
+     that the change takes effect.
+    """
+    mavlink_connection.param_set_send("GPS_TYPE", GPS_TYPE_NMEA)
+    mavlink_connection.reboot_autopilot()
 
 def send_fake_gps(mavlink_connection, lat=FAKE_LATITUDE, lon=FAKE_LONGITUDE, alt=FAKE_ALTITUDE):
     """Send a fake GPS message to test the vehicle in GUIDED mode indoors"""
