997f3164357d9ac2c344f45e77b787eeed2d7384
Dávid Máté <dm1e19@soton.ac.uk>
2024-01-06 19:45:26 +0000

    sexy

--- geometric_map.py
+++ geometric_map.py
@@ -60,6 +60,11 @@ def get_slope_grid(depth_image,depth_intrinsics,angles):
             plane_model, inliers = adjusted_pcd.segment_plane(distance_threshold=0.005,ransac_n=3,num_iterations=20000)
             [a, b, c, d] = plane_model
 
+            inlier_cloud = pcd.select_by_index(inliers)
+            inlier_cloud.paint_uniform_color([1.0, 0, 0])
+            outlier_cloud = pcd.select_by_index(inliers, invert=True)
+
+            print(outlier_cloud[:5])
             if a == 0:
                 pitch_degrees = 90
             else:
