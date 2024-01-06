27dd17b191ba9321e0c4f9b2f91064cbc6336e76
Dávid Máté <dm1e19@soton.ac.uk>
2024-01-06 15:52:44 +0000

    new way

--- geometric_map.py
+++ geometric_map.py
@@ -18,13 +18,25 @@ from matplotlib import pyplot as plt
 grid_n = 8
 grid_m = 8
 
+#!!!changed: 10x10 grid
+def direction_to_euler_angles(direction):
+    # Normalize the direction vector
+    normalized_direction = direction / np.linalg.norm(direction)
 
+    # Calculate pitch angle
+    pitch_rad = np.arcsin(normalized_direction[2])
+    pitch_deg = np.degrees(pitch_rad)
 
+    # Calculate yaw angle
+    yaw_rad = np.arctan2(normalized_direction[1], normalized_direction[0])
+    yaw_deg = np.degrees(yaw_rad)
+
+    return pitch_deg, yaw_deg
 
 def get_slope_grid(depth_image,depth_intrinsics,angles):
     rotation_matrix = o3d.geometry.get_rotation_matrix_from_zyx((math.radians(angles[0]),0,math.radians(angles[2])))
-    cell_height = int(depth_image.shape[0] // 8)
-    cell_width = int(depth_image.shape[1] // 8)
+    cell_height = int(depth_image.shape[0] // 10)
+    cell_width = int(depth_image.shape[1] // 10)
     depth_grid = scipy.sparse.bsr_matrix(depth_image, blocksize=(cell_height, cell_width), dtype=np.float32).data
     np_grid = np.asanyarray(depth_grid)
     i = 0
@@ -51,11 +63,12 @@ def get_slope_grid(depth_image,depth_intrinsics,angles):
             if a == 0:
                 pitch_degrees = 90
             else:
-                pitch_cos = math.cos(b / math.sqrt(math.pow(a, 2) + math.pow(b, 2) + math.pow(c, 2)))
-                print(pitch_cos)
-                pitch = math.acos(pitch_cos)
-                print(f'radians:{pitch}')
-                pitch_degrees = math.degrees(pitch)
+                # pitch_cos = math.cos(b / math.sqrt(math.pow(a, 2) + math.pow(b, 2) + math.pow(c, 2)))
+                # print(pitch_cos)
+                # pitch = math.acos(pitch_cos)
+                # print(f'radians:{pitch}')
+                # pitch_degrees = math.degrees(pitch)
+                pitch_degrees,_ = direction_to_euler_angles(np.array([a,b,c]))
                 print(f"{i} {j}: {a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0   pitch:{pitch_degrees}")
 
         else:
