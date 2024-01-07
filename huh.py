ac51eb5ba3da2c314af50cfd94bacb26c6626d06
Dávid Máté <dm1e19@soton.ac.uk>
2024-01-07 17:43:13 +0000

    sad

--- huh.py
+++ huh.py
@@ -1,3 +1,5 @@
+import copy
+import math
 import os
 import time
 from datetime import datetime
@@ -14,6 +16,17 @@ import camera_angle as cam
 from semantic_map import SemanticSegmentation
 logger = setup_custom_logger("navigation")
 import geometric_map as geo
+import open3d as o3d
+import numpy as np
+import pyrealsense2 as rs
+import time
+import scipy
+import cv2
+
+from matplotlib import pyplot as plt
+
+grid_n = 1
+grid_m = 4
 
 def apply_filters(depth_frame):
     decimation = rs.decimation_filter()
@@ -87,9 +100,7 @@ def get_new_images(frames):
     depth_frame = apply_filters(depth_frame)
     depth_image = np.asanyarray(depth_frame.get_data()) * depth_scale
 
-    # num_zeros = np.count_nonzero(depth_image == 0)
-    # logger.info(f"Number of zero values in depth image:{num_zeros}")
-    return depth_image, depth_image, color_image
+    return depth_image, depth_image, color_image,depth_frame
 
 
 def distance_to_obstacle(depth_image, slope_grid):
@@ -122,12 +133,127 @@ def distance_to_obstacle(depth_image, slope_grid):
 
     return min_distance
 
+
+def direction_to_euler_angles(direction):
+    # Normalize the direction vector
+    normalized_direction = direction / np.linalg.norm(direction)
+
+    # Calculate pitch angle
+    pitch_rad = np.arcsin(normalized_direction[2])
+    pitch_deg = np.degrees(pitch_rad)
+
+    # Calculate yaw angle
+    yaw_rad = np.arctan2(normalized_direction[1], normalized_direction[0])
+    yaw_deg = np.degrees(yaw_rad)
+
+    return pitch_deg, yaw_deg
+
+
+def pixel_to_3d_point(u, v, depth, intrinsics):
+    """
+    Convert pixel coordinates to 3D point coordinates.
+
+    Parameters:
+        u (float): Pixel x-coordinate.
+        v (float): Pixel y-coordinate.
+        depth (float): Depth value at the pixel coordinates.
+        intrinsics (dict): Camera intrinsics, including 'fx', 'fy', 'cx', and 'cy'.
+
+    Returns:
+        tuple: 3D point coordinates (X, Y, Z).
+    """
+    fx = intrinsics.fx
+    fy = intrinsics.fy
+    cx = intrinsics.ppx
+    cy = intrinsics.ppy
+
+    X = (u - cx) * depth / fx
+    Y = (v - cy) * depth / fy
+    Z = depth
+
+    return X, Y, Z
+
+
+def get_slope_grid_accurate(depth_image,angles):
+    grid_size = (1, 2)
+    grid_rows, grid_cols = grid_size
+    slope_grid = np.zeros(grid_size)
+    central_outliers = np.empty((0, 3))
+    rows, cols = depth_image.shape
+    # Calculate the size of each grid segment
+    segment_rows = rows // grid_rows
+    segment_cols = cols // grid_cols
+    rotation_matrix = o3d.geometry.get_rotation_matrix_from_zyx((math.radians(angles[0]),0,math.radians(angles[2])))
+    for n in range(grid_rows):
+        for m in range(grid_cols):
+            # Calculate the start and end indices for the current grid segment
+            start_row = n * segment_rows
+            end_row = (n + 1) * segment_rows
+            start_col = m * segment_cols
+            end_col = (m + 1) * segment_cols
+
+            # Get the original indices of the current grid segment
+            indices = np.array(np.meshgrid(range(start_row, end_row), range(start_col, end_col))).T.reshape(-1, 2)
+            print(indices)
+            # Initialize an empty array to store the results
+            points_3d = np.empty((len(indices), 3))
+            # Iterate through the pixel indices and apply the function
+            for i, (u, v) in enumerate(indices):
+                depth = depth_image[u, v]
+                print(depth)
+                point_3d = pixel_to_3d_point(u, v, depth, depth_intrinsics)
+                points_3d[i] = point_3d
+
+            # Create an Open3D PointCloud object
+            pcd = o3d.geometry.PointCloud()
+            # Set the points in the PointCloud
+            pcd.points = o3d.utility.Vector3dVector(points_3d)
+            pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])  # flip it
+            downpcd = pcd #.voxel_down_sample(voxel_size=0.02)
+            adjusted_pcd = copy.deepcopy(downpcd)
+            adjusted_pcd.rotate(rotation_matrix)
+
+            print(len(downpcd.points))
+            if len(downpcd.points)>100:
+                plane_model, inliers = adjusted_pcd.segment_plane(distance_threshold=0.005,ransac_n=3,num_iterations=1000)
+                [a, b, c, d] = plane_model
+                inlier_cloud = pcd.select_by_index(inliers)
+                inlier_cloud.paint_uniform_color([1.0, 0, 0])
+                outlier_cloud = pcd.select_by_index(inliers, invert=True)
+
+                if m == 1 or m == 2:    # if it's a central segment, should generalise this
+                    outlier_points = np.asarray(outlier_cloud.points)
+                    print(f'outliers shape:{outlier_points.shape}')
+                    central_outliers = np.vstack((central_outliers, outlier_points))
+
+                    # Find the index of the smallest number in each sublist
+                    min_indices = np.argmin(outlier_points, axis=0)
+
+                    # Extract the sublist with the smallest number for each element
+                    result_rows = outlier_points[min_indices, np.arange(outlier_points.shape[1])]
+                    print("\nLists with Smallest Numbers:")
+                    print(result_rows)
+                    print(f'nr of outlier points: {len(outlier_points)}')
+                if a == 0:
+                    pitch_degrees = 90
+                else:
+                    pitch_degrees,_ = direction_to_euler_angles(np.array([a,b,c]))
+                    print(f"{i} {j}: {a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0   pitch:{pitch_degrees}")
+            else:
+                pitch_degrees = 90
+            slope_grid[n, m] = pitch_degrees
+    print(central_outliers.shape)
+    # central_outliers[:, 2] *= -1
+    return slope_grid, central_outliers
+
+
 def new_obstacle_dist(depth_image,slope_grid, outlier_points):
+    slope_threshold = 30
     print(outlier_points[:10])
     min_distance = 999
     # Calculate the size of the central square
     central_width = depth_image.shape[1] // 4
-    central_height = depth_image.shape[0] // 40
+    central_height = depth_image.shape[0] // 20
 
     # Calculate the starting indices for the central square
     start_row = (depth_image.shape[0] - central_height) // 2
@@ -138,18 +264,42 @@ def new_obstacle_dist(depth_image,slope_grid, outlier_points):
     printed = False
     # Create a masked array where 0 values are masked
     masked_array = np.ma.masked_where(central_square == 0, central_square)
-    for i in range(start_row,start_row+central_height):
-        for j in range(start_col,start_col+central_width):
-            if depth_image[i,j] != 0:
-                dist = depth_image[i,j]
-                point = rs.rs2_deproject_pixel_to_point(depth_intrinsics, [i, j], dist)
-                rounded_point = np.round(point,decimals=2)
-                if not printed:
-                    print(rounded_point)
-                    printed = True
-                if rounded_point in outlier_points:
-                    print(f'pixel:{i} {j}')
-                    print(point)
+
+    if slope_grid[1]>slope_threshold or slope_grid>slope_threshold:
+        return 0.3
+    else:
+        for i in range(start_row,start_row+central_height):
+            for j in range(start_col,start_col+central_width):
+                if depth_image[i,j] != 0:
+                    dist = depth_image[i,j]
+                    point = rs.rs2_deproject_pixel_to_point(depth_intrinsics, [i, j], dist)
+                    rounded_point = np.round(point,decimals=2)
+                    if not printed:
+                        print(rounded_point)
+                        printed = True
+                    point_found = np.any(np.all(outlier_points == rounded_point, axis=1))
+                    if point_found:
+                        print(f'pixel:{i} {j}')
+                        print(point)
+
+
+# my_array = np.array([[1, 2, 3, 5],
+#                      [4, 5, 6, 5],
+#                      [7, 8, 9, 5]])
+# get_slope_grid_accurate(my_array,5)
+
+def get_slope_index(column, width):
+
+    index = math.ceil(column / (width / 4)) - 1
+    return index
+
+def percentage_of_elements_equal_to_value(arr, value):
+    total_elements = arr.size
+    matching_elements = np.count_nonzero(arr == value)
+    percentage = matching_elements / total_elements
+    return percentage
+
+
 # Main execution loop
 try:
 
@@ -165,24 +315,41 @@ try:
     depth_intrinsics = prof.as_video_stream_profile().get_intrinsics()
     cam.initialize_angle(frames)
     clf = SemanticSegmentation()
+    align_to = rs.stream.depth
+    align = rs.align(align_to)
+    pc = rs.pointcloud()
     while True:
+        print(f'principal point: {depth_intrinsics.ppx} {depth_intrinsics.ppy}')
         frames = pipeline.wait_for_frames()
-        steering_image, depth_image, color_image = get_new_images(frames)
+        frames = align.process(frames)
 
+        steering_image, depth_image, color_image,depth_frame = get_new_images(frames)
         angle = cam.get_camera_angle(frames)
         print(f'camera angle:{angle}')
 
-        start_time = time.time()
-        slope_grid,central_outlier_points = geo.get_slope_grid(depth_image,depth_intrinsics,angle)
-        print(slope_grid)
-        print("Slope Grid: --- %s seconds ---" % (time.time() - start_time))
         # start_time = time.time()
-        # mask = clf.get_semantic_map(color_image)
-        # logger.info("Segmenting image --- %s seconds ---" % (time.time() - start_time))
-        # # plt.imshow(mask)
-        # slope_grid = geo.get_slope_grid(depth_image,depth_intrinsics,angle)
+        # slope_grid,central_outlier_points = geo.get_slope_grid(depth_image,depth_intrinsics,angle)
         # print(slope_grid)
-        # # print(f'distance:{distance_to_obstacle(depth_image,slope_grid)}')
+        # print("Slope Grid: --- %s seconds ---" % (time.time() - start_time))
+
+        start_time = time.time()
+        slope_grid,central_outlier_points = get_slope_grid_accurate(depth_image,angle)
+        print(slope_grid)
+        print("Slope Grid(manual): --- %s seconds ---" % (time.time() - start_time))
+
+        ground_pitch_angle = slope_grid[get_slope_index(100,424)]
+
+
+        start_time = time.time()
+        mask = clf.get_semantic_map(color_image)
+        start_row = 0
+        central_square_height = 5
+        start_col = 10
+        central_square_width = 10
+        mask_square = mask[start_row:start_row + central_square_height, start_col:start_col + central_square_width]
+
+        print(ercentage_of_elements_equal_to_value(mask_square ,22))
+        # logger.info("Segmenting image --- %s seconds ---" % (time.time() - start_time))
         start_time = time.time()
         new_obstacle_dist(depth_image,1,central_outlier_points)
         print("Obstacle detection: --- %s seconds ---" % (time.time() - start_time))
