0d38fc0fe87d3b414aa8c1e11b263841fc25d05e
Dávid Máté <dm1e19@soton.ac.uk>
2024-01-15 15:32:31 +0000

    changed collision threshold speed

--- navigation_final.py
+++ navigation_final.py
@@ -585,7 +585,7 @@ def is_collision(current_speed):
 def is_collision2(current_speed):
     global target_speed
     vehicle_mode = mav_listener.get_mav_mode(mavlink_connection)
-    if current_speed < 0.2 and target_speed > 0:
+    if current_speed < 0.07 and target_speed > 0:
         target_speed = 0
         return True
     else:
